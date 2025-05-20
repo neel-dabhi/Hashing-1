a623ce1206e312262059924c0b33b57992e6fba9
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-05-20 16:16:03 -0300

    Completed Hashing 1

---
+++ GroupAnagrams.java
@@ -0,0 +1,27 @@
+// Time Complexity O(N x log k + n)
+// Space Complexity O(N)
+// Runs on LC : Yes
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedString = new String(chars);
+
+            if(map.get(sortedString) == null){
+                map.put(sortedString, new ArrayList<String>());
+            }
+            map.get(sortedString).add(str);
+        }
+
+        return new ArrayList<List<String>>(map.values());
+    }
+}
---
+++ WordPattern.java
@@ -0,0 +1,40 @@
+// Time Complexity O(1) Amortized
+// Space Complexity O(1)
+// Runs on LC : Yes
+
+import java.util.HashMap;
+
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> charToWord = new HashMap<>();
+        HashMap<String, Character> wordToChar = new HashMap<>();
+
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length)
+            return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            System.out.println(pattern.charAt(i) + " " + words[i]);
+
+            if (!charToWord.containsKey(pattern.charAt(i))) {
+                charToWord.put(pattern.charAt(i), words[i]);
+            }
+
+            if (!charToWord.get(pattern.charAt(i)).equals(words[i])) {
+                return false;
+            }
+
+            if (!wordToChar.containsKey(words[i])) {
+                wordToChar.put(words[i], pattern.charAt(i));
+            }
+
+            if (!wordToChar.get(words[i]).equals(pattern.charAt(i))) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+}
